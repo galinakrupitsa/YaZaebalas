c59f611878d1719d91a5144a82f3841b1cf145d4
Galina <galina.krupitsa@gmail.com>
2023-11-15 12:05:46 +0500

    buble soret

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-f="ghdtkj"
-print(f[1])
+import sys
+
+size = int(input('Введите размер массива: '))
+
+if size < 2:
+    print("сории, ничего не получится, введите другой размер массива1")
+    sys.exit()
+
+arr = []
+
+for i in range(size):
+    el = int(input(f'Введите элемент {i + 1}: '))
+    arr.append(el)
+
+print(arr)
+
+for i in range(size-1):
+    for j in range(size-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+
+print(arr)
